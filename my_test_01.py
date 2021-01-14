5bec0524264029cf43106a553030905972d58f81
Federico Ziliotto <fedezil@gmail.com>
2021-01-14 18:33:11 +0100

    my test commit

--- my_test_01.py
+++ my_test_01.py
@@ -10,18 +10,18 @@ import re
 
 
 # Collect and verify format for Primary WLC name and IP
-primaryWlcName = str(input("\nPrimary WLC name: "))
+primaryWlcName = input("\nPrimary WLC name: ")
 
 while not re.match(".*[a-zA-Z0-9\.\-\_].*", primaryWlcName): # Accepting characters, numbers, and some special characters ('.', '-' and '_')
-    primaryWlcName = str(input("Name not in a valid format. Please re-enter the primary WLC name: "))
+    primaryWlcName = input("Name not in a valid format. Please re-enter the primary WLC name: ")
 
-primaryWlcIp = str(input("Primary WLC IP: "))
+primaryWlcIp = input("Primary WLC IP: ")
 
 while not re.match("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", primaryWlcIp):
-    primaryWlcIp = str(input("IP address not in a valid format. Please re-enter the primary WLC IP: "))
+    primaryWlcIp = input("IP address not valid. Please re-enter the primary WLC IP: ")
 
 # Collect and verify format for Secondary WLC name and IP
-secondaryWlcName = str(input("\nSecondary WLC name (press Enter to skip): "))
+secondaryWlcName = input("\nSecondary WLC name (press Enter to skip): ")
 
 if len(secondaryWlcName) == 0: # If secondary WLC name is not specified, then automatically set name to 'null' and IP to '0.0.0.0' for both secondary and tertiary WLCs
     secondaryWlcName = "null"
@@ -31,15 +31,15 @@ if len(secondaryWlcName) == 0: # If secondary WLC name is not specified, then au
 
 else:
     while not re.match(".*[a-zA-Z0-9\.\-\_].*", secondaryWlcName): # Accepting characters, numbers, and some special characters ('.', '-' and '_')
-        secondaryWlcName = str(input("Name not in a valid format. Please re-enter the secondary WLC name: "))
+        secondaryWlcName = input("Name not in a valid format. Please re-enter the secondary WLC name: ")
 
-    secondaryWlcIp = str(input("Secondary WLC IP: "))
+    secondaryWlcIp = input("Secondary WLC IP: ")
 
     while not re.match("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", secondaryWlcIp):
-        secondaryWlcIp = str(input("IP address not in a valid format. Please re-enter the secondary WLC IP: "))
+        secondaryWlcIp = input("IP address not valid. Please re-enter the secondary WLC IP: ")
 
     # Collect and verify format for Tertiary WLC name and IP
-    tertiaryWlcName = str(input("\nTertiary WLC name (press Enter to skip): "))
+    tertiaryWlcName = input("\nTertiary WLC name (press Enter to skip): ")
 
     if len(tertiaryWlcName) == 0: # If tertiary WLC name is not specified, then automatically set name to 'null' and IP to '0.0.0.0' for the tertiary WLC
         tertiaryWlcName = "null"
@@ -47,12 +47,12 @@ else:
 
     else:
         while not re.match(".*[a-zA-Z0-9\.\-\_].*", tertiaryWlcName): # Accepting characters, numbers, and some special characters ('.', '-' and '_')
-            tertiaryWlcName = str(input("Name not in a valid format. Please re-enter the tertiary WLC name: "))
+            tertiaryWlcName = input("Name not in a valid format. Please re-enter the tertiary WLC name: ")
 
-        tertiaryWlcIp = str(input("Tertiary WLC IP: "))
+        tertiaryWlcIp = input("Tertiary WLC IP: ")
 
         while not re.match("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", tertiaryWlcIp):
-            tertiaryWlcIp = str(input("IP address not in a valid format. Please re-enter the tertiary WLC IP: "))
+            tertiaryWlcIp = input("IP address not valid. Please re-enter the tertiary WLC IP: ")
 
 # Print a summary of the settings to be pushed and ask the end user for a confirmation
 if secondaryWlcName == "null":
@@ -88,7 +88,7 @@ Tertiary WLC name: {}
 Tertiary WLC IP: {}
     """.format(primaryWlcName, primaryWlcIp, secondaryWlcName, secondaryWlcIp, tertiaryWlcName, tertiaryWlcIp))
 
-answer = str(input("Are you ok with these changes? (yes/no) "))
+answer = input("Are you ok with these changes? (yes/no) ")
 
 if re.match("no", answer, re.IGNORECASE):
     print("\nThank you, the script will stop here.\n")
